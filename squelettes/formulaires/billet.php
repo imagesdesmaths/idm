281b7ab621c06a2ce8d51e2e95cd2668502b341f
Vincent Beffara <vbeffara@ens-lyon.fr>
2008-12-23 18:11:08 +0100

    Use the new syntax for billet.html.

---
+++ squelettes/formulaires/billet.php
@@ -0,0 +1,48 @@
+<?php
+
+include_spip('base/abstract_sql');
+
+function formulaires_billet_charger () {
+  $valeurs = array('titre'=>'', 'texte'=>'', 'id_article'=>false);
+
+  return $valeurs;
+}
+
+function formulaires_billet_verifier () {
+  $erreurs = array();
+
+  foreach(array('titre','texte') as $obligatoire)
+    if (!_request($obligatoire))
+      $erreurs[$obligatoire] = "Le champ '$obligatoire' est obligatoire !";
+
+  if (!count($erreurs)) {
+    if (!$id_article = _request("id_article")) {
+      $id_article = sql_insertq ("spip_articles", array (
+        "id_rubrique" => 6,
+        "statut" => "tmp",
+        "accepter_forum" => "abo",
+        "date" => "NULL"));
+    }
+    $id_article = intval($id_article);
+
+    sql_updateq ('spip_articles',
+                 array ("titre" => _request("titre"), "texte" => _request("texte")),
+                 "id_article = $id_article");
+
+    if (!_request("ok")) $erreurs['id_article'] = $id_article;
+  }
+
+  return $erreurs;
+}
+
+function formulaires_billet_traiter () {
+  $id_article = intval(_request("id_article"));
+
+  sql_updateq ("spip_articles",
+    array ("statut" => "publie", "date" => "NOW()"),
+    "id_article = $id_article");
+
+  return array('message_ok' => "done");
+}
+
+?>
