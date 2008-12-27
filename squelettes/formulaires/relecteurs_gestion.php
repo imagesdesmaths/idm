7c85085d5b502ab7198b660c2fa60ab61d241adb
Vincent Beffara <vbeffara@ens-lyon.fr>
2008-12-27 19:26:59 +0100

    AJAXify the referee modification form.

--- squelettes/formulaires/relecteurs_gestion.php
+++ squelettes/formulaires/relecteurs_gestion.php
@@ -5,7 +5,7 @@ include_spip('base/abstract_sql');
 function formulaires_relecteurs_gestion_charger () {
   $valeurs = array();
   
-  $candidats = sql_allfetsel (array("id_auteur","math"), "spip_auteurs", "role = 'candidat'");
+  $candidats = sql_allfetsel (array("id_auteur","math"), "spip_auteurs", "role = 'candidat' OR role = 'relecteur'");
   foreach ($candidats as $row) {
     $id = $row["id_auteur"];
     $valeurs["decision_$id"] = "oui";
@@ -21,12 +21,17 @@ function formulaires_relecteurs_gestion_verifier () {
 }
 
 function formulaires_relecteurs_gestion_traiter () {
-  $candidats = sql_allfetsel (array("id_auteur"), "spip_auteurs", "role = 'candidat'");
+  $candidats = sql_allfetsel (array("id_auteur","math","role"), "spip_auteurs", "role = 'candidat' OR role = 'relecteur'");
   foreach ($candidats as $row) {
     $id = $row["id_auteur"];
-    $decision = _request("decision_$id");
-    if ($decision == "oui") sql_updateq ("spip_auteurs", array("role"=>"relecteur", "math" => _request("math_$id")), "id_auteur=$id");
-    if ($decision == "non") sql_updateq ("spip_auteurs", array("role"=>"visiteur", "math" => _request("math_$id")), "id_auteur=$id");
+    if ($row["role"] == "candidat") {
+      $decision = _request("decision_$id");
+      if ($decision == "oui") sql_updateq ("spip_auteurs", array("role"=>"relecteur", "math" => _request("math_$id")), "id_auteur=$id");
+      if ($decision == "non") sql_updateq ("spip_auteurs", array("role"=>"visiteur", "math" => _request("math_$id")), "id_auteur=$id");
+    } else {
+      if (_request("math_$id") != $row["math"]) sql_updateq ("spip_auteurs", array("math"=>_request("math_$id")), "id_auteur=$id");
+      if (_request("exterminate_$id"))          sql_updateq ("spip_auteurs", array("role"=>"visiteur"), "id_auteur=$id");
+    }
   }
 }
 
