53f72d7af32ddbf9e43c45834111b53970550a92
Vincent Beffara <vbeffara@ens-lyon.fr>
2009-01-29 19:33:02 +0100

    Publish the day after tomorrow, or one week later.

--- squelettes/formulaires/billet.php
+++ squelettes/formulaires/billet.php
@@ -43,12 +43,21 @@ function formulaires_billet_traiter () {
   $id_article = intval(_request("id_article"));
   $id_auteur = $GLOBALS['auteur_session']['id_auteur'];
   
+  $today = time();
+  $today -= $today % (24*3600); // Midnight this morning
+
   $previous = sql_getfetsel ("UNIX_TIMESTAMP(date)",
     "spip_auteurs_articles,spip_articles",
     "(spip_articles.id_article=spip_auteurs_articles.id_article) AND (id_auteur=$id_auteur) AND (statut='publie')",
     array(), "date DESC", "1");
+  $previous -= $previous % (24*3600); // Their last contribution to the site
 
-  $when = max (time(), $previous + 7*24*3600);
+  $when = max ($today + 2*24*3600, $previous + 7*24*3600); // Publish the day after tomorrow, with one week minimum for the same contributor.
+
+  // TODO : here, check for backlog and the like, while (more than 2) 
+  // add 24 hours.
+
+  $when += 7*3600; // Publish at 7:00 AM
 
   sql_updateq ("spip_articles",
     array ("statut" => "publie", "date" => date("Y-m-d H:i:s", $when)),
