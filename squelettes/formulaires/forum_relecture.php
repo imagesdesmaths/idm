ff54b1b8a0c3698dd3542828a25049fcc59e39a3
Vincent Beffara <vbeffara@ens-lyon.fr>
2009-11-03 17:44:53 +0100

    Notify Christine when a new message is posted.

--- squelettes/formulaires/forum_relecture.php
+++ squelettes/formulaires/forum_relecture.php
@@ -1,5 +1,7 @@
 <?php
 
+include_spip('inc/envoyer_mail');
+
 function formulaires_forum_relecture_charger ($id_article, $id_parent) {
   $id_article = intval ($id_article);
   $valeurs = array('titre'      => sql_getfetsel ('titre', 'spip_articles', "id_article = $id_article"),
@@ -21,22 +23,35 @@ function formulaires_forum_relecture_verifier ($id_article, $id_parent) {
 }
 
 function formulaires_forum_relecture_traiter ($id_article, $id_parent) {
-  $id_article = intval ($id_article);
-  $id_parent  = intval ($id_parent);
   $id_auteur = intval ($GLOBALS['auteur_session']['id_auteur']);
-  $auteur = sql_getfetsel ('nom', 'spip_auteurs', "id_auteur = $id_auteur");
+  $auteur    = sql_getfetsel ('nom', 'spip_auteurs', "id_auteur = $id_auteur");
 
   sql_insertq ('spip_forum', array (
-    'id_parent'  => $id_parent,
-    'id_article' => $id_article,
-    'date_heure' => 'NOW()',
-    'titre'      => _request("titre"),
-    'texte'      => _request("texte"),
-    'auteur'     => $auteur,
+    'id_parent'  => intval ($id_parent),
+    'id_article' => intval ($id_article),
     'id_auteur'  => $id_auteur,
+    'auteur'     => $auteur,
+    'titre'      => _request ('titre'),
+    'texte'      => _request ('texte'),
+    'date_heure' => 'NOW()',
     'statut'     => 'relmod'
-    ));
+  ));
 
+  $mail_to      = "huyghe@math.u-strasbg.fr";
+  $mail_subject = "Un nouveau message de relecture sur IdM";
+  $mail_text    = <<< END
+Bonjour Christine !
+
+Un nouveau commentaire a été posté dans un forum de relecture. Tu peux
+aller le lire et le valider sur la page habituelle, à savoir :
+
+  http://images.math.cnrs.fr/spip.php?page=moderation
+
+-- 
+Le robot de "Images des maths"
+END;
+
+  inc_envoyer_mail_dist ($mail_to, $mail_subject, utf8_encode($mail_text));
   return array ('message_ok' => "done");
 }
 
