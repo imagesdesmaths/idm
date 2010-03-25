5e6af1eb01db7c9dc7957fbc0ef9c3371421cfab
Vincent Beffara <vbeffara@ens-lyon.fr>
2010-03-25 16:16:23 +0100

    Ooops, one could not become a referee anymore.

--- plugins/IdM/formulaires/relecteurs_perso.php
+++ plugins/IdM/formulaires/relecteurs_perso.php
@@ -41,6 +41,9 @@ function formulaires_relecteurs_perso_verifier () {
 function formulaires_relecteurs_perso_traiter () {
   $id_auteur = $GLOBALS['auteur_session']['id_auteur'];
 
+  if (!sql_countsel ('spip_idm_relecteurs', "id_auteur = $id_auteur"))
+    sql_insertq ('spip_idm_relecteurs', array("id_auteur" => $id_auteur));
+
   if (_request('inscription')) {
     sql_updateq ('spip_idm_relecteurs',
       array ('role' => 'candidat', 'math' => supprimer_tags(_request('qui'))),
