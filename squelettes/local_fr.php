1c9e230f25a9295e44445ed0d7d485d7d9d6339e
Vincent Beffara <vbeffara@ens-lyon.fr>
2010-07-18 20:09:09 -0300

    Move the message overrides to the right squelettes/local_fr.php place.

---
+++ squelettes/local_fr.php
@@ -0,0 +1,9 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+ #'form_forum_probleme_mail' => 'Votre identifiant vient de vous &ecirc;tre envoy&eacute;.', #There was an email problem, let's hope for the best ...
+  'form_forum_voici2'        => 'Voici vos identifiants pour proposer des articles sur le site "@nom_site_spip@" (@adresse_site@) :', #KEEP
+  'forum_qui_etes_vous'      => 'Qui &ecirc;tes-vous ?', #KEEP
+);
+?>
