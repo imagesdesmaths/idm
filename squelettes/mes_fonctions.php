a6e40cf945e2fe76f937e854b8e7feb13a769f91
Vincent Beffara <vbeffara@ens-lyon.fr>
2010-11-11 21:33:00 +0100

    Made the lettrine() function a bit more robust. Closes #23.

--- squelettes/mes_fonctions.php
+++ squelettes/mes_fonctions.php
@@ -6,8 +6,8 @@ $tables_principales['spip_auteurs']['field']['billettiste'] = "enum('oui','non')
 function lettrine ($texte) {
   $lines = explode ("\n", $texte);
   for ($i=0; $i<count($lines); $i++) {
-    if (preg_match ('/(.*)<p>[\\s]*([A-Za-z]|&#171;)([A-Za-zç]*)/', $lines[$i], $matches)) {
-      $lettrine = $matches[1] . '<p class="spip lettrine"><span class="lettrine"><span class="lettrine_first">' . $matches[2] . '</span>' . $matches[3] . '</span>';
+    if (preg_match ('/(.*)<p>[\\s]*((&#171;|.)(’)?)([^\\s]*)([\\s])/', $lines[$i], $matches)) {
+      $lettrine = $matches[1] . '<p class="spip lettrine"><span class="lettrine"><span class="lettrine_first">' . $matches[2] . '</span>' . $matches[5] . '</span>' . $matches[6];
       $lines[$i] = str_replace ($matches[0], $lettrine, $lines[$i]);
       break;
     } 
