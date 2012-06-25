14a210850c9ca97778e72464ca184cb9d7a4dcc1
Vincent Beffara <vbeffara@ens-lyon.fr>
2012-06-25 22:59:50 +0400

    Upgrade iextras to version 3.0.3

--- plugins/champs_extras2_interface/lang/paquet-iextras_en.php
+++
@@ -1,8 +0,0 @@
-<?php
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-
-	'iextras_titre' => 'Extra fields (Interface)',
-	'iextras_slogan' => 'Interface to manage the extra fields on SPIP objects',
-	'iextras_description' => '',
-);
-?>
--- plugins/champs_extras2_interface/lang/paquet-iextras_it.php
+++
@@ -1,8 +0,0 @@
-<?php
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-
-	'iextras_titre' => 'Campi Extra (Interfaccia)',
-	'iextras_slogan' => 'Interfaccia per gestire i campi extra',
-	'iextras_description' => '',
-);
-?>
