692dd964887d4fa5b18930c9ad65ad43c50c9458
Vincent Beffara <vbeffara@ens-lyon.fr>
2012-06-26 15:24:52 +0400

    PHPMailer cures all our e-mail problems, apparently.

---
+++ plugins/auto/facteur/v2.2.1/lang/paquet-facteur_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/facteur/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'facteur_description' => 'Facteur s\'occupe de la distribution des courriels au format HTML, texte ou mixte ; via SMTP ou non',
+	'facteur_nom' => 'Facteur',
+	'facteur_slogan' => 'Il distribue vos courriels'
+);
+
+?>
