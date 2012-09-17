896dd3fe7b80f7b3a5dbb4b9119d838f456fc2ff
Vincent Beffara <vbeffara@ens-lyon.fr>
2012-09-17 11:12:04 +0200

    Second version, ready for release

--- squelettes/mes_fonctions.php
+++ squelettes/mes_fonctions.php
@@ -13,34 +13,9 @@ function trunctext($texte, $longeur_max)
     return $texte;
 }
 
-function critere_compteur_publie($idb, &$boucles, $crit){
- $op='';
- $boucle = &$boucles[$idb];
- $params = $crit->param;
- $type = array_shift($params);
- $type = $type[0]->texte;
- if(preg_match(',^(\w+)([<>=])([0-9]+)$,',$type,$r)){
-     $type=$r[1];
-     $op=$r[2];
-     $op_val=$r[3];
- }
- $type_id = 'compt.id_'.$type;
- $type_requete = $boucle->type_requete;
- $id_table = $boucle->id_table . '.' . $boucle->primary;
- $boucle->select[]= 'COUNT('.$type_id.') AS compteur_'.$type;
- $boucle->from['compt']="spip_".$type;
- $boucle->where[]= array("'='", "'".$id_table."'", "'compt.".$boucle->primary."'");
- $boucle->where[]= array("'='", "'compt.statut'" , "'\"publie\"'"); 
- $boucle->group[]=$id_table;
- if ($op)
-     $boucle->having[]= array("'".$op."'", "'compteur_".$type."'",$op_val);
 
-} 
 
-function balise_COMPTEUR_FORUM_dist($p) {
-   $p->code = '$Pile[$SP][\'compteur_forum\']';
-   $p->interdire_scripts = false;
-   return $p;
-} 
+a
+
 
 ?>
