5ba0e00aef7522a26ba9fabb5b951a6081c10297
Vincent Beffara <vbeffara@ens-lyon.fr>
2013-02-20 14:59:55 +0100

    Add a default value to the 'active' field of spip_idm_relecture

--- plugins/IdM/base/idm.php
+++ plugins/IdM/base/idm.php
@@ -45,7 +45,7 @@ function idm_declarer_tables_auxiliaires ($tables) {
                                                            'action'     => "ENUM ('born', 'affected', 'read', 'comment', 'complete', 'lazy', 'silent')",
                                                            'id_article' => "BIGINT(21) NOT NULL",
                                                            'id_forum'   => "BIGINT(21) NOT NULL",
-                                                           'active'     => "ENUM ('no', 'yes')"),
+                                                           'active'     => "ENUM ('no', 'yes') DEFAULT 'yes'"),
                                          'key' => array());
 
   $tables['spip_idm_teams'] = array ('field' => array ('team'      => "TINYTEXT",
