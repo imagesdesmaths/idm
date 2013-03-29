c08e64a2340ceaa44bb5b17bc894c15d5aab1553
Vincent Beffara <vbeffara@ens-lyon.fr>
2013-03-29 14:50:52 +0100

    Put new relecteurs in 'nouveau'

--- plugins/IdM/formulaires/relecteurs_gestion.php
+++ plugins/IdM/formulaires/relecteurs_gestion.php
@@ -5,7 +5,7 @@ include_spip('inc/filtres');
 
 function formulaires_relecteurs_gestion_charger () {
   $valeurs = array();
-  
+
   $candidats = sql_allfetsel (array("id_auteur","math"), "spip_idm_relecteurs", "role = 'candidat' OR role = 'relecteur'");
   foreach ($candidats as $row) {
     $id = $row["id_auteur"];
@@ -28,7 +28,7 @@ function formulaires_relecteurs_gestion_traiter () {
     if ($row["role"] == "candidat") {
       $decision = _request("decision_$id");
       if ($decision == "oui") {
-        sql_updateq ("spip_idm_relecteurs", array("role"=>"relecteur", "math" => supprimer_tags(_request("math_$id"))), "id_auteur=$id");
+        sql_updateq ("spip_idm_relecteurs", array("role"=>"relecteur", "categorie"=>"nouveau", "math" => supprimer_tags(_request("math_$id"))), "id_auteur=$id");
         sql_insertq ("spip_idm_relecture", array("id_auteur"=>intval($id), "action"=>"born"));
       }
       if ($decision == "non") sql_updateq ("spip_idm_relecteurs", array("role"=>"visiteur", "math" => supprimer_tags(_request("math_$id"))), "id_auteur=$id");
