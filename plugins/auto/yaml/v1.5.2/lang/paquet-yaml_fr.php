88f680760e1772d531b96423f1ba786b1d4194d7
Vincent Beffara <vbeffara@ens-lyon.fr>
2015-02-08 18:32:04 +0100

    Upgrade 'yaml' to 1.5.2

--- plugins/auto/yaml/v1.5.2/lang/paquet-yaml_fr.php
+++ plugins/auto/yaml/v1.5.2/lang/paquet-yaml_fr.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// Y
-	'yaml_description' => 'Ce plugin fournit les fonctions de lecture/écriture du format YAML:
+	'yaml_description' => 'Ce plugin fournit les fonctions de lecture/écriture du format YAML :
 	<code>yaml_decode()</code> et <code>yaml_encode()</code>. Il fournit aussi le format yaml pour la boucle (DATA).',
 	'yaml_slogan' => 'Un format de fichier simple pour éditer des listes de données'
 );
