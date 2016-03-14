74903d52210b4195a8577cd0aa14a77e3296a44e
image <image@myIdm.univ-littoral.fr>
2016-03-14 14:23:43 +0100

    Autorise le 'preview' d'un article en rédaction pour tout le monde et pas que pour l'auteur.

--- ecrire/base/objets.php
+++ ecrire/base/objets.php
@@ -179,7 +179,7 @@ function lister_tables_objets_sql($table_sql = null, $desc = array()) {
 					array(
 						'champ' => 'statut',
 						'publie' => 'publie',
-						'previsu' => 'publie,prop,prepa/auteur',
+						'previsu' => 'publie,prop,prepa',
 						'post_date' => 'date',
 						'exception' => array('statut', 'tout')
 					)
