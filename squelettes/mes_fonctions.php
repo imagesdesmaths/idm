1e1f7af420ff5f505289fdbe1e5b1b4274d051f3
Jean Fromentin <jean.fromentin@gmail.com>
2017-08-28 11:20:52 +0200

    Inconnu

--- squelettes/mes_fonctions.php
+++ squelettes/mes_fonctions.php
@@ -1,9 +1,9 @@
 <?php 
 
 function trunctext($texte, $longeur_max) {
-    if (strlen($texte) > $longeur_max)
+if (strlen($texte) > $longeur_max)
     {
-    $texte = substr($texte, 0, $longeur_max);
+    $texte = substr($texte, 0, $longueur_max);
     $dernier_espace = strrpos($texte, " ");
     $texte = substr($texte, 0, $dernier_espace)."...";
     }
